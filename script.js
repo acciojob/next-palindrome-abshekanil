779c6ef485dda5cbac218918aa884835c8033902
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-09 12:33:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 function nextPalindrome(num) {
   //your JS code here.
+	num = parseInt(num);
+
+	num++;
+
+	while(true)
+		{
+			let strNum = num.toString();
+
+			let i = 0;
+			let j = strNum.length - 1;
+			let isPallindrome = true;
+
+			while(i<j)
+				{
+					if(strNum[i] !== strNum[j])
+					{
+						isPallindrome = false;
+						break;
+					}
+
+					i++;
+					j--;
+				}
+
+			if(isPallindrome)
+			{
+				return num;
+			}
+			else{
+				num++;
+			}
+			
+		}
+	
+ 
 }
 
 const input = prompt("Enter a palindrome number");
